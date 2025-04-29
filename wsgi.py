b7e50a91ab35451fa32c7fd6d1ff7f5c57097184
isabelaleite <isabela.leeite@hotmail.com>
2025-04-28 23:17:01 -0300

    fix: change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,3 @@ if os.getenv("FLASK_ENV") == "development":
     app = create_app('config.DevConfig')
 else:
     app = create_app('config.ProdConfig')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(host='0.0.0.0', port=port)
